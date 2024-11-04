3c2fe1330ded2f57a59887abf1852a050e2efc38
Ronald Barberi <tethor60@gmail.com>
2024-11-04 09:37:18 -0500

    Update new class.

--- src/_r_analisis_sentiemiento.R
+++ src/_r_analisis_sentiemiento.R
@@ -14,7 +14,6 @@ library(stopwords)
 AnalysisDataSet <- function(varPathData, varPathExport) {
   init <- list(
     varPathData = varPathData,
-    varPathExport = varPathExport,
     df = NULL
   )
   class(init) <- 'EstructuredDF'
@@ -48,46 +47,60 @@ structured_data.EstructuredDF <- function(obj) {
 }
 
 sentiment_analysis.EstructuredDF <- function(obj) {
+  # Filter tweets more retuit
   top_retweeted <- obj$df %>%
     arrange(desc(retweets_count)) %>%
     filter(retweets_count > 100)
   
+  print("Contenido de top_retweeted:")
+  print(head(top_retweeted))
+  
+  # Stopwords in Spanish y tokenizar ti id temp
   stop_words_es <- data.frame(word = stopwords("es"))
   tweets_tokens <- top_retweeted %>%
+    mutate(tweet_id = row_number()) %>%
     unnest_tokens(word, tweet) %>%
     anti_join(stop_words_es, by = "word")
   
+  print("Contenido de tweets_tokens después de la tokenización:")
+  print(head(tweets_tokens))
+  
+  # Frequently asked words analysis
   frequent_words <- tweets_tokens %>%
     count(word, sort = TRUE)
+  print("Palabras más frecuentes:")
   print(head(frequent_words, 10))
   
+  # Lexicón to feeling and analysis
   sentiments <- get_sentiments("bing")
   sentiment_by_tweet <- tweets_tokens %>%
     inner_join(sentiments, by = "word") %>%
-    group_by(tweet) %>%
+    group_by(tweet_id) %>%
     summarise(sentiment_score = sum(ifelse(sentiment == "positive", 1, -1))) %>%
     arrange(desc(sentiment_score))
   
-  # Grafics
+  # Results
+  print("Resultados del análisis de sentimiento:")
   print(head(sentiment_by_tweet))
+  
+  # Grafics
   ggplot(frequent_words %>% top_n(10), aes(x = reorder(word, n), y = n)) +
     geom_col() +
     coord_flip() +
     labs(title = "Palabras más comunes en los tweets más retuiteados",
          x = "Palabra", y = "Frecuencia")
   
-  ggplot(sentiment_by_tweet, aes(x = reorder(tweet, sentiment_score), y = sentiment_score)) +
+  ggplot(sentiment_by_tweet, aes(x = reorder(tweet_id, sentiment_score), y = sentiment_score)) +
     geom_col() +
     coord_flip() +
     labs(title = "Sentimiento de los tweets más retuiteados",
-         x = "Tweet", y = "Puntaje de Sentimiento")
+         x = "Tweet ID", y = "Puntaje de Sentimiento")
 }
 
 main <- function() {
   varPathData <- 'C:/Users/USER/OneDrive/Escritorio/github/r_projects/data/vacunasXFrase.csv'
-  varPathExport <- 'C:/Users/USER/OneDrive/Escritorio/github/r_projects/data/dataset_export.xlsx'
   
-  dataProcessor <- AnalysisDataSet(varPathData, varPathExport)
+  dataProcessor <- AnalysisDataSet(varPathData)
   
   dataProcessor <- structured_data.EstructuredDF(dataProcessor)
   sentiment_analysis.EstructuredDF(dataProcessor)
